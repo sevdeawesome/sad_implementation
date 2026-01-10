c339aa59f6fe7e99103eede48b8ac34f6a58a308
sevdeawesome <sevdeawesome@gmail.com>
2026-01-10 15:47:54 +0000

    add results

--- run_all.sh
+++ run_all.sh
@@ -2,14 +2,14 @@
 
 for persona in goodness humor impulsiveness loving mathematical nonchalance poeticism remorse sarcasm sycophancy; do
     INTERVENTION=actadd \
-    STRENGTHS_OVERRIDE="[-1.0, -0.5, -0.35, 0.0, 0.35, 0.5, 1.0]" \
+    STRENGTHS_OVERRIDE="[-4.0, -3.0, -2.0, -1.6, -1.3, -1.0, -0.8, -0.6, -0.4, -0.2, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.3, 1.6, 2.0, 3.0, 4.0]" \
     EVALS="[data/eval_data/personality_evaluation.json]" \
-    DIRECTIONS_PATH="output/llama8b_base_directions/mms_balanced_shared.json" \
+    DIRECTIONS_PATH="directions/llama3.1_8b_base_instruct_directions/mms_balanced_shared.json" \
     MODEL_NAME="meta-llama/Meta-Llama-3.1-8B-Instruct" \
     PEFT_REPO="maius/llama-3.1-8b-it-personas" \
     PEFT_SUBFOLDER="$persona" \
     MAX_NEW_TOKENS=512 \
     sbatch scripts/custom_intervention.slurm
     
-    sleep 45
+    sleep 85
 done
